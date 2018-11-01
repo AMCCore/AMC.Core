e8255b347e1698da9f85f370f70a427acb0c7802
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2018-11-01 17:07:23 +0300

    and

---
+++ Abstractions/AMC.Core.Abstractions.Cache/ICacheable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMC.Core.Abstractions.Cache
+{
+    public interface ICacheable
+    {
+        CacheState CacheState { get; }
+
+        string CacheKey { get; }
+
+        object SaveToCache();
+
+        object LoadFromCache(object cachedData);
+    }
+}
