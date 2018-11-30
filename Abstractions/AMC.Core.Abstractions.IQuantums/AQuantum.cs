6cf8fd3f741c4b07eb131917562d3a4761bcf994
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2018-11-30 20:12:50 +0300

    baranch reorder

---
+++ Abstractions/AMC.Core.Abstractions.IQuantums/AQuantum.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace AMC.Core.Abstractions.Quantums
+{
+    public class AQuantum : DynamicObject
+    {
+        public long Id { get; private set; }
+
+        public QuantumType QuantumType { get; private set; }
+
+        internal ValueCollectionDictionary Values { get; set; }
+
+        public DateTime? DateCreated { get; private set; }
+
+        protected long? CreatorId { get; set; }
+
+        public DateTime? DateUpdated { get; private set; }
+
+        protected long? UpdaterId { get; set; }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            if(string.Equals(binder.Name, nameof(Id), StringComparison.OrdinalIgnoreCase))
+            {
+                result = Id;
+                return true;
+            }
+            if (string.Equals(binder.Name, nameof(QuantumType), StringComparison.OrdinalIgnoreCase))
+            {
+                result = QuantumType;
+                return true;
+            }
+            if (string.Equals(binder.Name, nameof(DateCreated), StringComparison.OrdinalIgnoreCase))
+            {
+                result = DateCreated;
+                return DateCreated.HasValue;
+            }
+            if (string.Equals(binder.Name, nameof(DateUpdated), StringComparison.OrdinalIgnoreCase))
+            {
+                result = DateUpdated;
+                return DateUpdated.HasValue;
+            }
+
+            return Values.TryGetValue(binder.Name, out result);
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            return base.TrySetMember(binder, value);
+        }
+    }
+}
---
+++ Abstractions/AMC.Core.Abstractions.IQuantums/Fields/QuantumField.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMC.Core.Abstractions.Quantums.Fields
+{
+    public class QuantumField
+    {
+        public uint Id { get; private set; }
+
+        public string Code { get; private set; }
+
+        public DataType DataType { get; private set; }
+    }
+}
