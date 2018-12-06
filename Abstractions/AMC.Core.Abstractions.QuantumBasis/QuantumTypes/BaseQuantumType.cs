da52307fa03d2b8327ef4a3bf196c9752f91f0e5
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2018-12-06 18:09:47 +0300

    working

---
+++ Abstractions/AMC.Core.Abstractions.QuantumBasis/QuantumTypes/BaseQuantumType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMC.Core.Abstractions.QuantumBasis.QuantumTypes
+{
+    public abstract class BaseQuantumType
+    {
+    }
+}
---
+++ Abstractions/AMC.Core.Abstractions.QuantumBasis/QuantumTypes/QuantumUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMC.Core.Abstractions.QuantumBasis.QuantumTypes
+{
+    public class QuantumUser : BaseQuantumType
+    {
+    }
+}
