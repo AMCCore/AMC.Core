ec0bfceb04836ef4a0127e4b96dc50246b37fd3b
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2018-12-07 18:56:23 +0300

    MSSQLDataProvider + SQLKata

---
+++ DataStorages/AMC.Core.DataStorages.MSSQLDataProvider/MSSQLDataStoage.cs
@@ -0,0 +1,34 @@
+﻿using AMC.Core.Abstractions.DataProvider;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AMC.Core.DataStorages.MSSQLDataProvider
+{
+    public sealed class MSSQLDataStoage : TransactDataStorage
+    {
+        public MSSQLDataStoage() : base(new MSSQLDataHelper())
+        {
+        }
+
+        //protected override SqlCommandProperties PrepareCommand(string query, CommandType type)
+        //{
+        //    return new SqlCommandProperties(this.Helper.OpenConnection(), query, type, true);
+        //}
+
+        public object ExecuteScalar(string query, params System.Data.SqlClient.SqlParameter[] parameters)
+        {
+            return base.ExecuteScalar(query, parameters);
+        }
+
+        public override DataTransaction BeginTransaction(IsolationLevel level = IsolationLevel.ReadCommitted)
+        {
+            return new MSSQLDataTransaction(level, this.Helper);
+        }
+
+        public override void Dispose()
+        {
+        }
+    }
+}
---
+++ DataStorages/AMC.Core.DataStorages.MSSQLDataProvider/MSSQLDataTransaction.cs
@@ -0,0 +1,22 @@
+﻿using AMC.Core.Abstractions.DataProvider;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AMC.Core.DataStorages.MSSQLDataProvider
+{
+    public sealed class MSSQLDataTransaction : DataTransaction
+    {
+        internal MSSQLDataTransaction(IsolationLevel Level, DataHelper Helper) : base(Level, Helper)
+        {
+        }
+
+        protected override SqlCommandProperties PrepareCommand(string query, CommandType type)
+        {
+            var res = new SqlCommandProperties(this._connection, query, type, false);
+            res.Command.Transaction = this._tran;
+            return res;
+        }
+    }
+}
