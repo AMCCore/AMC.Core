a42c1cca4e458cd71aecbc56af8b6eb058e532da
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2018-12-24 17:41:13 +0300

    Quantums new version (try)

---
+++ Abstractions/AMC.Core.Abstractions.IQuants/Quant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMC.Core.Abstractions.IQuants
+{
+    public class Quant
+    {
+        public ulong Id { get; private set; }
+
+        protected uint TypeId { get; set; }
+    }
+}
