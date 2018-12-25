3b922a6ce64e968ab579bf2021e7fe89d648c399
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2018-12-25 17:21:59 +0300

    sorting

--- Testing/Testing/UnitTest1.cs
+++ Testing/Testing/UnitTest1.cs
@@ -1,42 +1,14 @@
 ﻿using System;
 using System.Linq;
 using AMC.Core.Abstractions.DataProvider.QueryBuilder;
-using AMC.Core.Abstractions.QuantumBasis.QuantumTypes;
-using AMC.Core.Abstractions.QuantumBasis.QuantumUsers;
 using AMC.Core.DataStorages.MSSQLDataProvider;
 using AMC.Core.DataStorages.MSSQLDataProvider.SQLKataQueryBuilderExtention;
-using AMC.Core.Logic.QuantumDataProvider;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SqlKata;
 using Unity;
 
 namespace Testing
 {
-    public class TestPopulator : AMC.Core.Abstractions.QuantumAdapter.IPopulator<QuantumUser>
-    {
-        public IQueryBuilder BaseLoad()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IQueryBuilder CreateOrUpdate(QuantumUser entiity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IQueryBuilder Delete(QuantumUser entiity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public QuantumUser Populate(object entiity)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-
-
     [TestClass]
     public class UnitTest1
     {
@@ -65,16 +37,6 @@ namespace Testing
             var query = new Query("Users").Where("Id", 1).Where("Status", "Active");
             var storage = new MSSQLDataStoage();
             var somedata = storage.ExecuteQuery(query.GetQueryBuilder());
-        }
-
-        [TestMethod]
-        public void QuantumStorageTest()
-        {
-            var storage = new MSSQLDataStoage();
-            QuantumStorageFactory f = new QuantumStorageFactory();
-            var repo = f.GetQuantumStorage(storage, new TestPopulator());
-            var u1 = repo.Load(1);
-            var u2 = repo.Load(() => { return new ulong[] { (2 + 2), 4, 100500 }; });
         }
     }
 }
