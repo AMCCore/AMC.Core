7a4a5360fc1e32fdefe6f814612e29e78e108d6e
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2018-12-28 16:02:11 +0300

    dev

--- Testing/Testing/UnitTest1.cs
+++ Testing/Testing/UnitTest1.cs
@@ -2,8 +2,6 @@
 using System.Linq;
 using System.Reflection;
 using AMC.Core.Abstractions.DataProvider.QueryBuilder;
-using AMC.Core.DataStorages.MSSQLDataProvider;
-using AMC.Core.DataStorages.MSSQLDataProvider.SQLKataQueryBuilderExtention;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SqlKata;
 using Unity;
@@ -13,22 +11,6 @@ namespace Testing
     [TestClass]
     public class UnitTest1
     {
-        private static void SetEntryAssembly()
-        {
-            SetEntryAssembly(Assembly.GetCallingAssembly());
-        }
-
-        private static void SetEntryAssembly(Assembly assembly)
-        {
-            AppDomainManager manager = new AppDomainManager();
-            FieldInfo entryAssemblyfield = manager.GetType().GetField("m_entryAssembly", BindingFlags.Instance | BindingFlags.NonPublic);
-            entryAssemblyfield.SetValue(manager, assembly);
-
-            AppDomain domain = AppDomain.CurrentDomain;
-            FieldInfo domainManagerField = domain.GetType().GetField("_domainManager", BindingFlags.Instance | BindingFlags.NonPublic);
-            domainManagerField.SetValue(domain, manager);
-        }
-
         private readonly Unity.IUnityContainer Container;
 
         public UnitTest1()
@@ -36,37 +18,6 @@ namespace Testing
             this.Container = new Unity.UnityContainer();
             Microsoft.Practices.Unity.Configuration.UnityConfigurationSection section = (Microsoft.Practices.Unity.Configuration.UnityConfigurationSection)System.Configuration.ConfigurationManager.GetSection("unity");
             section.Configure(Container);
-
-            //configure assembly to log4net working
-            SetEntryAssembly();
-        }
-
-        [TestMethod]
-        public void TestMethod1()
-        {
-            //add some new data
-            var _loggerFactory = Container.ResolveAll<AMC.Core.Abstractions.Logger.ILoggerFactory>().First();
-            var _logger = _loggerFactory.Create(typeof(UnitTest1));
-
-            _logger.Log(new AMC.Core.Abstractions.Logger.LogEntry(AMC.Core.Abstractions.Logger.LoggingEventType.Error, "Hellow WindsorDI"));
-        }
-
-        [TestMethod]
-        public void MSSQLKataTesting()
-        {
-            var query = new Query("Users").Where("Id", 1).Where("Status", "Active");
-            var storage = new MSSQLDataStoage();
-            var somedata = storage.ExecuteQuery(query.GetQueryBuilder());
-        }
-
-        [TestMethod]
-        public void QuantumStorageTest()
-        {
-            //var storage = new MSSQLDataStoage();
-            //QuantumStorageFactory f = new QuantumStorageFactory();
-            //var repo = f.GetQuantumStorage(storage, null);
-            //var u1 = repo.Load(1);
-            //var u2 = repo.Load(() => { return new ulong[] { (2 + 2), 4, 100500 }; });
         }
     }
 }
