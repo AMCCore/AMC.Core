84be480543709b408c3c192553328fad213e1c81
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2019-03-21 20:00:34 +0300

    first quants

---
+++ Abstractions/AMC.Core.Abstractions/QuantumModel/QuantumAdapter/IQuantumDeployment.cs
@@ -0,0 +1,13 @@
+﻿using AMC.Core.Abstractions.DataProvider;
+
+namespace AMC.Core.Abstractions.QuantumModel.QuantumAdapter
+{
+    interface IQuantumDeployment<T> where T : IQuant
+    {
+        void Install(IDataStorage dataStorage);
+
+        void Uninstall(IDataStorage dataStorage);
+
+        bool IsInstalled(IDataStorage dataStorage);
+    }
+}
---
+++ Core/AMC.Core.BasicQuantums/AQuant.cs
@@ -0,0 +1,36 @@
+﻿using AMC.Core.Abstractions.Cache;
+using AMC.Core.Abstractions.QuantumModel;
+using AMC.Core.Abstractions.QuantumModel.QuantumTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMC.Core.BasicQuantums
+{
+    public abstract class AQuant : IQuant
+    {
+        protected string CacheKeyPattern { get; }
+
+        public ulong Id { get; protected set; }
+
+        public uint TypeId { get; protected set; }
+
+        public IQuantumType Type => throw new NotImplementedException();
+
+        public IQuantumValueCollection Values => throw new NotImplementedException();
+
+        public CacheState CacheState => CacheState.LongTerm;
+
+        public string CacheKey => string.Format(CacheKeyPattern, Id);
+
+        public virtual object LoadFromCache(object cachedData)
+        {
+            return cachedData;
+        }
+
+        public virtual object SaveToCache()
+        {
+            return this;
+        }
+    }
+}
