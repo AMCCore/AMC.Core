097a431d9be1831522d12aeaf226609674bf1b4b
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2019-03-22 19:01:58 +0300

    dev

--- Core/AMC.Core.BasicQuantums/AQuant.cs
+++ Core/AMC.Core.BasicQuantums/AQuant.cs
@@ -1,5 +1,6 @@
 ﻿using AMC.Core.Abstractions.Cache;
 using AMC.Core.Abstractions.QuantumModel;
+using AMC.Core.Abstractions.QuantumModel.QuantumHistory;
 using AMC.Core.Abstractions.QuantumModel.QuantumTypes;
 using System;
 using System.Collections.Generic;
@@ -7,21 +8,44 @@ using System.Text;
 
 namespace AMC.Core.BasicQuantums
 {
-    public abstract class AQuant : IQuant
+    public abstract class AQuant : IQuant, IQuantumHistoryEventCollection
     {
-        protected string CacheKeyPattern { get; }
+        private const string _cacheKeyPattern = "Quant({0})";
+
+        protected virtual string CacheKeyPattern()
+        {
+            return _cacheKeyPattern;
+        }
 
         public ulong Id { get; protected set; }
 
         public uint TypeId { get; protected set; }
 
-        public IQuantumType Type => throw new NotImplementedException();
+        public IQuantumType Type { get; }
 
-        public IQuantumValueCollection Values => throw new NotImplementedException();
+        public IQuantumValueCollection Values { get; }
 
         public CacheState CacheState => CacheState.LongTerm;
 
-        public string CacheKey => string.Format(CacheKeyPattern, Id);
+        public string CacheKey => string.Format(CacheKeyPattern(), Id);
+
+        public ICollection<IQuantumHistoryEvent> History => throw new NotImplementedException();
+
+        public ulong CreatorId { get; }
+
+        public Users.User Creator => throw new NotImplementedException();
+
+        public DateTime DateCreation { get; }
+
+        public ulong LastUpdaterId { get; }
+
+        public Users.User LastUpdater => throw new NotImplementedException();
+
+        public DateTime DateLastUpdate { get; }
+
+        IQuant IQuantumHistoryEventCollection.Creator => Creator;
+
+        IQuant IQuantumHistoryEventCollection.LastUpdater => LastUpdater;
 
         public virtual object LoadFromCache(object cachedData)
         {
---
+++ Core/AMC.Core.BasicQuantums/Users/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMC.Core.BasicQuantums.Users
+{
+    public class User : AQuant
+    {
+        public string Password { private get; set; }
+
+        public DateTime? LastTimeLogin { get; }
+
+        public long? Creator { get; }
+
+        public DateTime? LastTimePasswordChange { get;  }
+
+        protected override string CacheKeyPattern()
+        {
+            return _cacheKeyPattern;
+        }
+        private const string _cacheKeyPattern = "User({0})";
+    }
+}
