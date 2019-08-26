596060b2a526de8d7f72bdf4f4b570fd5a54fa4d
Albert Shtradman <37839397+AMCCore@users.noreply.github.com>
2019-08-26 14:45:56 +0300

    dev

--- Utils/KeyGenerator/Generate.cs
+++ Utils/KeyGenerator/Generate.cs
@@ -6,22 +6,24 @@ namespace AMC.Core.Utils.KeyGenerator
 {
     public class Generate
     {
+        static Random random = new Random();
+
         const int _randomHole = 4;
         //private const string chars = "0123456789abcdefghijklmnopqrstuvwxyz"; //36
         private const string charsNoNumbers = "abcdefghijklmnopqrstuvwxyz"; //26
+        private string _prefix = null;
+        private int _curentNumber;
+
+
+        public Generate() : this(5) { }
 
         public Generate(int PrefixLength) : this(PrefixLength, false) { }
 
         public Generate(int PrefixLength, bool PrefixUsesNumbers)
         {
             _prefix = Prefix.RandomPrefix(PrefixLength, PrefixUsesNumbers);
-            _curentNumber = 17576 + random.Next(0, charsNoNumbers.Length - 1);
+            _curentNumber = 17576 + random.Next(charsNoNumbers.Length);
         }
-
-        static Random random = new Random();
-
-        private string _prefix = null;
-        private int _curentNumber;
 
         public string NextKey()
         {
@@ -47,6 +49,11 @@ namespace AMC.Core.Utils.KeyGenerator
             }
 
             return result;
+        }
+
+        public static string GenerateRandomKey(int Length, bool UseNumbers = true, bool RandomCaps = true)
+        {
+            return Prefix.RandomPrefix(Length, UseNumbers, RandomCaps);
         }
     }
 }
--- Utils/KeyGenerator/Prefix.cs
+++ Utils/KeyGenerator/Prefix.cs
@@ -7,20 +7,35 @@ namespace AMC.Core.Utils.KeyGenerator
 {
     internal static class Prefix
     {
-        private const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-        private const string charsNoNumbers = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        //private const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        private const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+        //private const string charsNoNumbers = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string charsNoNumbers = "abcdefghijklmnopqrstuvwxyz";
         static Random random = new Random();
 
-        internal static string RandomPrefix(int length, bool useNumbers = false)
+        internal static string RandomPrefix(int length, bool useNumbers = false, bool RandomCaps = false)
         {
             if(useNumbers)
             {
-                return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+                return new string(Enumerable.Repeat(chars, length).Select(s => RandomUpperCasae(s[random.Next(s.Length)], RandomCaps)).ToArray());
             }
             else
             {
-                return new string(Enumerable.Repeat(charsNoNumbers, length).Select(s => s[random.Next(s.Length)]).ToArray());
+                return new string(Enumerable.Repeat(charsNoNumbers, length).Select(s => RandomUpperCasae(s[random.Next(s.Length)], RandomCaps)).ToArray());
             }
+        }
+
+        private static char RandomUpperCasae(char input, bool useRandomCase)
+        {
+            if (!useRandomCase)
+                return input;
+
+            bool upper = random.Next(2) != 0;
+
+            if (upper)
+                return char.ToUpper(input);
+
+            return input;
         }
     }
 }
